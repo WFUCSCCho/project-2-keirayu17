113d82250e228016b1b97664a48e102d254ed9b3
Keira Yu <keira_yuyi@outlook.com>
2024-10-23 22:05:05 -0400

    Added Codes.

--- src/AvlTree.java
+++ src/AvlTree.java
@@ -51,7 +51,29 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * @return the new root of the subtree.
      */
     private AvlNode<AnyType> remove( AnyType x, AvlNode<AnyType> t ) {
-	// FINISH ME
+        if ( t == null ) {
+            return t;
+        }
+        int compareResult = x.compareTo(t.element);
+        if ( compareResult < 0 ) {
+            t.left = remove( x, t.left );
+        }
+        else if ( compareResult > 0 ) {
+            t.right = remove( x, t.right );
+        }
+        else if ( t.left != null && t.right != null ) {
+            t.element = findMin(t.right).element;
+            t.right = remove(t.element, t.right);
+        }
+        else {
+            if ( t.left != null ) {
+                t = t.left;
+            }
+            else{
+                t = t.right;
+            }
+        }
+        return balance(t);
     }
 
     /**
@@ -112,7 +134,27 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
 
     // Assume t is either balanced or within one of being balanced
     private AvlNode<AnyType> balance( AvlNode<AnyType> t ) {
-	// FINISH ME
+        if ( t == null ){
+            return t;
+        }
+        if (height(t.left) - height(t.right) > ALLOWED_IMBALANCE){
+            if (height(t.left.left) >= height(t.left.right)){
+                t = rotateWithLeftChild(t);
+            }
+            else{
+                t = doubleWithLeftChild(t);
+            }
+        }
+        else if(height(t.right) - height(t.left) > ALLOWED_IMBALANCE){
+            if (height(t.right.right) >= height(t.right.left)){
+                t = rotateWithRightChild(t);
+            }
+            else{
+                t = doubleWithRightChild(t);
+            }
+        }
+        t.height = Math.max(height(t.left), height(t.right)) + 1;
+        return t;
     }
 
     public void checkBalance( ) {
@@ -142,7 +184,20 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * @return the new root of the subtree.
      */
     private AvlNode<AnyType> insert( AnyType x, AvlNode<AnyType> t ) {
-	// FINISH ME
+        if (t == null) {
+            return new AvlNode<>(x, null, null);
+        }
+        int compareResult = x.compareTo( t.element );
+        if ( compareResult < 0 ){
+            t.left = insert( x, t.left );
+        }
+        else if ( compareResult > 0 ){
+            t.right = insert( x, t.right );
+        }
+        else {
+            ; // Do nothing
+        }
+        return balance( t );
     }
 
     /**
@@ -151,7 +206,13 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * @return node containing the smallest item.
      */
     private AvlNode<AnyType> findMin( AvlNode<AnyType> t ) {
-	// FINISH ME
+        if ( t == null ){
+            return null;
+        }
+        else if (t.left == null ){
+            return t;
+        }
+        return findMin( t.left );
     }
 
     /**
@@ -160,7 +221,13 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * @return node containing the largest item.
      */
     private AvlNode<AnyType> findMax( AvlNode<AnyType> t ) {
-	// FINISH ME
+        if ( t == null ){
+            return null;
+        }
+        else if (t.right == null ){
+            return t;
+        }
+        return findMax( t.right );
     }
 
     /**
@@ -170,7 +237,19 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * @return true if x is found in subtree.
      */
     private boolean contains( AnyType x, AvlNode<AnyType> t ) {
-	// FINISH ME
+        if ( t == null ){
+            return false;
+        }
+        int compareResult = x.compareTo( t.element );
+        if ( compareResult < 0 ){
+            return contains( x, t.left );
+        }
+        else if ( compareResult > 0 ){
+            return contains( x, t.right );
+        }
+        else {
+            return true;
+        }
     }
 
     /**
@@ -178,7 +257,11 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * @param t the node that roots the tree.
      */
     private void printTree( AvlNode<AnyType> t ) {
-	// FINISH ME
+        if( t != null ){
+            printTree( t.left );
+            System.out.println( t.element );
+            printTree( t.right );
+        }
     }
 
     /**
@@ -194,7 +277,12 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * Update heights, then return new root.
      */
     private AvlNode<AnyType> rotateWithLeftChild( AvlNode<AnyType> k2 ) {
-	// FINISH ME
+        AvlNode<AnyType> k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+        k2.height = Math.max( height( k2.left ), height( k2.right ) ) + 1;
+        k1.height = Math.max( height( k1.left ), k2.height ) + 1;
+        return k1;
     }
 
     /**
@@ -203,7 +291,12 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * Update heights, then return new root.
      */
     private AvlNode<AnyType> rotateWithRightChild( AvlNode<AnyType> k1 ) {
-	// FINISH ME
+        AvlNode<AnyType> k2 = k1.right;
+        k1.right = k2.left;
+        k2.left = k1;
+        k1.height = Math.max( height( k1.left ), height( k1.right ) ) + 1;
+        k2.height = Math.max( height( k2.right ), k1.height ) + 1;
+        return k2;
     }
 
     /**
@@ -213,7 +306,8 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * Update heights, then return new root.
      */
     private AvlNode<AnyType> doubleWithLeftChild( AvlNode<AnyType> k3 ) {
-	// FINISH ME
+        k3.left = rotateWithRightChild( k3.left );
+        return rotateWithLeftChild( k3 );
     }
 
     /**
@@ -223,7 +317,8 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
      * Update heights, then return new root.
      */
     private AvlNode<AnyType> doubleWithRightChild( AvlNode<AnyType> k1 ) {
-	// FINISH ME
+        k1.right = rotateWithLeftChild( k1.right );
+        return rotateWithRightChild( k1 );
     }
 
     private static class AvlNode<AnyType> {
--- src/Proj2.java
+++ src/Proj2.java
@@ -1,6 +1,20 @@
+/***
+ * @file: Proj2.java
+ * @description: Main class for the project. It creates an arraylist to store all data entries
+ *               from the movie dataset. Contains sorted and shuffled versions of the arraylist
+ *               for further running time analysis.
+ *               Data source: https://www.kaggle.com/datasets/mohammadrizwansajjad/top-200-movies-of-2023
+ *               Data has been modified by eliminating unnecessary attributes and trimming spaces to
+ *               avoid confusion
+ * @author: Keira Yu
+ * @date: October 24, 2024
+ */
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Collections;
@@ -24,10 +38,122 @@ public class Proj2 {
         inputFileNameStream = new FileInputStream(inputFileName);
         inputFileNameScanner = new Scanner(inputFileNameStream);
 
-        // ignore first line
+        // Ignore first line
         inputFileNameScanner.nextLine();
 
-	// FINISH ME
+        // Store data into original ArrayList
+        ArrayList<Movie> movieList = new ArrayList<>();
 
+        while (inputFileNameScanner.hasNextLine() && movieList.size() < numLines) {
+            String line = inputFileNameScanner.nextLine();
+            String[] data = line.split(",");
+            String title = data[0];
+            int totalGross = Integer.parseInt(data[1]);
+            String releaseDate = data[2];
+            String distributor = data[3];
+
+            Movie movie = new Movie(title, totalGross, releaseDate, distributor);
+            movieList.add(movie);
+        }
+        inputFileNameScanner.close();
+
+        // Create sorted list
+        ArrayList<Movie> sortedList = new ArrayList<>(movieList);
+        Collections.sort(sortedList);
+
+        // Create shuffled list
+        ArrayList<Movie> shuffledList = new ArrayList<>(movieList);
+        Collections.shuffle(shuffledList);
+
+        BST<Movie> sortedBST = new BST<>();
+        BST<Movie> shuffledBST = new BST<>();
+        AvlTree<Movie> sortedAVL = new AvlTree<>();
+        AvlTree<Movie> shuffledAVL = new AvlTree<>();
+
+        FileWriter fileWriter = new FileWriter("output.txt", true);
+        long startTime, endTime, duration;
+
+
+        // Insert SORTED into BST
+        startTime = System.nanoTime();
+        for (Movie movie : sortedList) {
+            sortedBST.insert(movie);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("BST Sorted Insertion Time: " + duration + " nanoseconds");
+        fileWriter.append(numLines + ",BST,Sorted,Insertion," + duration + "\n");
+
+        // Insert SHUFFLED into BST
+        startTime = System.nanoTime();
+        for (Movie movie : shuffledList) {
+            shuffledBST.insert(movie);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("BST Shuffled Insertion Time: " + duration + " nanoseconds");
+        fileWriter.append(numLines + ",BST,Shuffled,Insertion," + duration + "\n");
+
+        // Insert SORTED into AVL
+        startTime = System.nanoTime();
+        for (Movie movie : sortedList) {
+            sortedAVL.insert(movie);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("AVL Sorted Insertion Time: " + duration + " nanoseconds");
+        fileWriter.append(numLines + ",AVL,Sorted,Insertion," + duration + "\n");
+
+        // Insert SHUFFLED into AVL
+        startTime = System.nanoTime();
+        for (Movie movie : shuffledList) {
+            shuffledAVL.insert(movie);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("AVL Shuffled Insertion Time: " + duration + " nanoseconds");
+        fileWriter.append(numLines + ",AVL,Shuffled,Insertion," + duration + "\n");
+
+        // Search in SORTED BST
+        startTime = System.nanoTime();
+        for (Movie movie: movieList){
+            Movie searchInSortedBST = sortedBST.search(movie);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("BST Sorted Search Time: " + duration + " nanoseconds");
+        fileWriter.append(numLines + ",BST,Sorted,Search," + duration + "\n");
+
+        // Search in SORTED AVL
+        startTime = System.nanoTime();
+        for (Movie movie : movieList) {
+            boolean searchInSortedAVL = sortedAVL.contains(movie);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("AVL Sorted Search Time: " + duration + " nanoseconds");
+        fileWriter.append(numLines + ",AVL,Sorted,Search," + duration + "\n");
+
+        // Search in SHUFFLED BST
+        startTime = System.nanoTime();
+        for (Movie movie: movieList){
+            Movie searchInShuffledBST = shuffledBST.search(movie);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("BST Shuffled Search Time: " + duration + " nanoseconds");
+        fileWriter.append(numLines + ",BST,Shuffled,Search," + duration + "\n");
+
+        // Search in SHUFFLED AVL
+        startTime = System.nanoTime();
+        for (Movie movie : movieList) {
+            boolean searchInShuffledAVL = shuffledAVL.contains(movie);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println("AVL Shuffled Search Time: " + duration + " nanoseconds");
+        fileWriter.append(numLines + ",AVL,Shuffled,Search," + duration + "\n");
+
+        fileWriter.close();
     }
 }
